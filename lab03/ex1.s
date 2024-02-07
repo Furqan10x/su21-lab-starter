b5c18f311361b47f66f770ad6cdebda6d4f72369
Furqan10x <furqan.liaquat@10xengineers.ai>
2024-02-07 16:14:09 +0500

    lab 03 Tasks updated

--- lab03/ex1.s
+++ lab03/ex1.s
@@ -2,7 +2,7 @@
 .word 2, 4, 6, 8
 n: .word 9
 
-.text
+.text 
 main:
     add t0, x0, x0
     addi t1, x0, 1
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 11
 
 .text
 main:
@@ -9,7 +9,7 @@ main:
     lw a0, 0(t0)
     jal ra, factorial
 
-    addi a1, a0, 0
+    addi a1, t0, 0
     addi a0, x0, 1
     ecall # Print Result
 
@@ -21,4 +21,22 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+       # Function prologue
+    addi sp, sp, -4      # Allocate space on the stack
+    sw ra, 0(sp)         # Save return address on the stack
+
+    # Function body
+    li t0, 1            # Initialize t0 (result) to 1
+    li t1, 1          # Initialize t1 (counter) to 1
+
+loop:
+    blt a0, t1, end_loop  # Exit loop if t1 >= a0
+    mul t0, t0, t1       # Multiply result by counter
+    addi t1, t1, 1       # Increment counter
+    j loop               # Jump back to loop
+
+end_loop:
+    # Function epilogue
+    lw ra, 0(sp)         # Restore return address from the stack
+    addi sp, sp, 4       # Deallocate space on the stack
+    jr ra                # Jump back to the return address
